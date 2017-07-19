3cf3b04713e60c2feda32e0377f9fc842c5b8c2e
Pranaycul <pranaycul@gmail.com>
2017-07-19 16:25:10 +0530

    this is new update

---
+++ website/02_pointer/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func zero(x int) {
+	fmt.Println("value of x  is ", x)
+	fmt.Println("address of x is ", &x)
+	x = 0
+}
+
+func main() {
+	x := 5
+	zero(x)
+	fmt.Println("Value of x is ", x)
+	fmt.Println("address of x is ", &x)
+}
