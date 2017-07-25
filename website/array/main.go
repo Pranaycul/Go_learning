7cb2e449fdd2277de570c0c245eccd486ed76efd
Pranaycul <pranaycul@gmail.com>
2017-07-25 18:14:38 +0530

    25th afternoon

--- website/array/main.go
+++ website/array/main.go
@@ -3,15 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var x [58]string
-	fmt.Println(x)
+	var x [258]int
+
 	fmt.Println(len(x))
 	fmt.Println(x[42])
 
-	for i := 65; i <= 122; i++ {
-		x[i-65] = string(i)
+	for i := 0; i <= 257; i++ {
+		x[i] = i
 	}
-	fmt.Println(x)
-	fmt.Println(len(x))
-	fmt.Println(x[42])
+	for i, v := range x {
+		fmt.Printf("%v - %T - %b\n", v, v, v)
+		if i > 50 {
+			break
+		}
+	}
 }
