2457ef5cf3d133dc1e63f095ed8487f2667c8cad
Pranaycul <pranaycul@gmail.com>
2017-07-25 18:17:49 +0530

    25th afternoon

--- website/array/main.go
+++ website/array/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var x [258]int
+	var x [258]byte
 
 	fmt.Println(len(x))
 	fmt.Println(x[42])
 
 	for i := 0; i <= 257; i++ {
-		x[i] = i
+		x[i] = byte(i)
 	}
 	for i, v := range x {
 		fmt.Printf("%v - %T - %b\n", v, v, v)
