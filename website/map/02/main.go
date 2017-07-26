446df898bdbebe6d9decdeb2727cdb583041e9ed
Pranaycul <pranaycul@gmail.com>
2017-07-26 14:16:37 +0530

    26th afternoon

---
+++ website/map/02/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var myGreeting = map[string]string{}
+	myGreeting["Tim"] = "Good morning."
+	myGreeting["Jenny"] = "Bonjour."
+
+	fmt.Println(myGreeting)
+	fmt.Println(myGreeting == nil)
+}
