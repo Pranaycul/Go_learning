23ae856571c8548023c2f9838625fe40ff447688
Pranaycul <pranaycul@gmail.com>
2017-07-27 14:44:48 +0530

    27th update

--- website/struct/02/main.go
+++ website/struct/02/main.go
@@ -1,3 +1,40 @@
 package main
 
+import "fmt"
 
+type Person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+type Doublezero struct {
+	Person
+	First         string
+	LicenseToKill bool
+}
+
+func main() {
+
+	p1 := Doublezero{
+		Person: Person{
+			First: "James",
+			Last:  "Bond",
+			Age:   30,
+		},
+		First:         "Double zero Seven",
+		LicenseToKill: true,
+	}
+	p2 := Doublezero{
+		Person: Person{
+			First: "Miss",
+			Last:  "MoneyPenny",
+			Age:   19,
+		},
+		First:         "If looks could kill",
+		LicenseToKill: false,
+	}
+
+	fmt.Println(p1.First, " ", p1.Person.First)
+	fmt.Println(p2.First, " ", p2.Person.First)
+}
