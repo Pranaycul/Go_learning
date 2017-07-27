a5ab1fcee0c89e611f6d43f84a25d5e41b63fd01
Pranaycul <pranaycul@gmail.com>
2017-07-27 14:30:12 +0530

    27th update

--- website/struct/01/main.go
+++ website/struct/01/main.go
@@ -11,9 +11,9 @@ func main() {
 	}
 
 	p1 := person{"Pranay", "Wankhede", 29}
-	p2 := person{"Phoenix", "Ash-maker", 0}
+	p2 := person{first: "Phoenix", last: "Ash-maker", age: 0}
 
 	fmt.Println(p1.first, " ", p1.last, " ", p1.age)
-	fmt.Println(p2.first, " ", p2.last, " ", p1.age)
+	fmt.Println(p2.first, " ", p2.last, " ", p2.age)
 
 }
